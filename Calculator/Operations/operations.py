10cae17219ab9b9fbbf9e861e00393e3c9a2b259
Chinonso Anyanka <cc792@njit.edu>
2021-07-06 11:46:12 -0400

    Added operations.py

---
+++ Calculator/Operations/operations.py
@@ -0,0 +1,31 @@
+class Operations:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def addition(a, b):
+        return float(a) + float(b)
+
+    @staticmethod
+    def subtraction(a, b):
+        return float(b) - float(a)
+
+    @staticmethod
+    def multiplication(a, b):
+        return round(float(a) * float(b), 9)
+
+    @staticmethod
+    def division(a, b):
+        try:
+            return round(float(b) / float(a), 9)
+        except ZeroDivisionError:
+            return "Error: can't divide by zero"
+
+    @staticmethod
+    def square(a):
+        return float(a) ** 2
+
+    @staticmethod
+    def square_root(a):
+        return round((float(a) ** .5), 8)
