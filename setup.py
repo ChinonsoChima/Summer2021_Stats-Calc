951df6f68fbb3a8432584006d915e5a3108a9bd1
Chinonso Anyanka <cc792@njit.edu>
2021-07-06 11:34:40 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='Calculator',
+      version='0.0.1',
+      description='It is a calculator',
+      author='',
+      author_email='',
+      url='',
+      packages=find_packages(),
+     )
